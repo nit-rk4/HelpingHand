34b1905c1416ccb85d20cf2d7483cc552c830c36
Ysabel Delos Santos <140307129+d455s@users.noreply.github.com>
2025-07-04 03:47:11 +0800

    Navbar
    > Added navbar.php
    - Responsive: Yes
    - NOTE: Please add
    <?php include("navbar.php"); ?>
    to the <body> of every webpage
    
    > Updated style.css
    - Added navbar styling
    - Backup font not yet set

---
+++ pages/navbar.php
@@ -0,0 +1,13 @@
+<body>
+    <div class="navbar" id="nav-color">
+        <img class="logo" src="assets/logo.svg" alt="logo">
+        <nav>
+            <ul class="nav-links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">About us</a></li>
+            </ul>
+        </nav>
+        <a class="searchbar"><input type="text" placeholder="Search..."></a>
+    </div>
+</body>
