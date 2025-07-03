7439dd78d68227bb6d4f099f10241a19ee71f08a
Ysabel Delos Santos <140307129+d455s@users.noreply.github.com>
2025-07-04 04:01:17 +0800

    Help board (WIP)
    !! WIP !!
    > Added help_board.php
    - Responsive: Not completely
    
    > Updated style.css
    - Added help board styling
    
    Todo:
    - Add images
    - Adjust margins of contents
    - Fix long box text
    - Improve responsiveness

---
+++ pages/help_board.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Help Board</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <?php include("navbar.php"); ?>
+
+    <main class="request-grid">
+        <article class="request bg-yellow short-box grid-col-span-2 ">
+            <img src="" alt="">
+            <div>
+                <h2 class="request-title">Title</h2>
+                <p class="author text-yellow">Requester Name</p>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. In, ex, inventore quasi commodi deleniti corporis magni delectus culpa beatae mollitia est suscipit tempore harum aliquid. Consectetur, quam! Fugit, placeat ea.</p>
+        </article>
+        <article class="request bg-blue short-box">
+            <div>
+                <img src="" alt="">
+            </div>
+            <div>
+                <h2 class="request-title">Title</h2>
+                <p class="author text-blue">Requester Name</p>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. In, ex, inventore quasi commodi deleniti corporis magni delectus culpa beatae mollitia est suscipit tempore harum aliquid. Consectetur, quam! Fugit, placeat ea.</p>
+        </article>
+        <article class="request bg-green long-box">
+            <div>
+                <img src="" alt="">
+            </div>
+            <div>
+                <h2 class="request-title">Title</h2>
+                <p class="author text-green">Requester Name</p>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. In, ex, inventore quasi commodi deleniti corporis magni delectus culpa beatae mollitia est suscipit tempore harum aliquid. Consectetur, quam! Fugit, placeat ea.</p>
+        </article>
+        <article class="request bg-yellow short-box">
+            <img src="" alt="">
+            <div>
+                <h2 class="request-title">Title</h2>
+                <p class="author text-yellow">Requester Name</p>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. In, ex, inventore quasi commodi deleniti corporis magni delectus culpa beatae mollitia est suscipit tempore harum aliquid. Consectetur, quam! Fugit, placeat ea.</p>
+        </article>
+        <article class="request bg-blue short-box grid-col-span-2">
+            <div>
+                <img src="" alt="">
+            </div>
+            <div>
+                <h2 class="request-title">Title</h2>
+                <p class="author text-blue">Requester Name</p>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. In, ex, inventore quasi commodi deleniti corporis magni delectus culpa beatae mollitia est suscipit tempore harum aliquid. Consectetur, quam! Fugit, placeat ea.</p>
+        </article>
+        
+    </main>
+    
+</body>
+</html>
