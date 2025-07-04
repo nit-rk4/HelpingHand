4e3a1420e0eee4e6513c8e5ce29f78f77a548865
Denise Ruth Manalang <manalangdeniseruth@gmail.com>
2025-07-04 21:06:59 +0800

    Updated the file paths

--- pages/admin_requests.php
+++ pages/admin_requests.php
@@ -3,14 +3,14 @@
 <head>
   <meta charset="UTF-8" />
   <title>HelpingHand Admin - User Requests</title>
-  <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" href="../css/style.css" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
 <body>
   
   <header class="navbar" id="nav-color">
     <div class="nav-left">
-      <img class="logo" src="assets/logo.svg" alt="HelpingHand Logo" />
+      <img class="logo" src="../assets/logo.svg" alt="HelpingHand Logo" />
 
       <nav>
         <ul class="nav-links">
