45b6272541d6a9c2a79855f1580423ab5a196d44
Ysabel Delos Santos <140307129+d455s@users.noreply.github.com>
2025-07-05 02:31:12 +0800

    Uploaded files, updated style.css
    help_board.php
    - shows up to 7 requests per page
    
    request_1.php
    - sample of individual request pages from help_board
    
    style.css update
    - added styling for help_board.php and request_1.php

---
+++ pages/help_board.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Help Board</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <?php include("navbar.php"); ?>
+
+    <h1 class="page-header">Help board</h1>
+
+    <main class="helpboard-grid">
+        <article class="helpboard bg-yellow short-box grid-col-span-2">
+            <div class="helpboard-content">
+                <img src="images/sample1.jpg" alt="sample" class="helpboard-image">
+                <div class="helpboard-text">
+                    <h2 class="helpboard-title"><a href="request_1.php">Title</a></h2>
+                    <p class="author text-yellow">Requester Name</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. In, ex, inventore quasi commodi deleniti corporis magni delectus culpa beatae mollitia est suscipit tempore harum aliquid.</p>
+                </div>
+            </div>
+        </article>
+
+        <article class="helpboard bg-blue short-box">
+            <div class="helpboard-content">
+                <img src="images/sample2.jpg" alt="sample" class="helpboard-image">
+                <div class="helpboard-text">
+                    <h2 class="helpboard-title"><a href="#">Title</a></h2>
+                    <p class="author text-blue">Requester Name</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. In, ex, inventore quasi commodi deleniti corporis magni delectus culpa beatae mollitia est suscipit tempore harum aliquid.</p>
+                </div>
+            </div>
+        </article>
+
+        <article class="helpboard bg-green long-box">
+            <div class="helpboard-content">
+                <img src="images/sample3.jpg" alt="sample" class="helpboard-image">
+                <div class="helpboard-text">
+                    <h2 class="helpboard-title"><a href="#">Title</a></h2>
+                    <p class="author text-green">Requester Name</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. In, ex, inventore quasi commodi deleniti corporis magni delectus culpa beatae mollitia est suscipit tempore harum aliquid.</p>
+                </div>
+            </div>
+        </article>
+
+        <article class="helpboard bg-yellow short-box grid-col-span-2">
+            <div class="helpboard-content">
+                <img src="images/sample4.jpg" alt="sample" class="helpboard-image">
+                <div class="helpboard-text">
+                    <h2 class="helpboard-title"><a href="#">Title</a></h2>
+                    <p class="author text-yellow">Requester Name</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. In, ex, inventore quasi commodi deleniti corporis magni delectus culpa beatae mollitia est suscipit tempore harum aliquid.</p>
+                </div>
+            </div>
+        </article>
+
+        <article class="helpboard bg-blue short-box grid-col-span-2">
+            <div class="helpboard-content">
+                <img src="images/sample5.jpg" alt="sample" class="helpboard-image">
+                <div class="helpboard-text">
+                    <h2 class="helpboard-title"><a href="#">Title</a></h2>
+                    <p class="author text-blue">Requester Name</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. In, ex, inventore quasi commodi deleniti corporis magni delectus culpa beatae mollitia est suscipit tempore harum aliquid.</p>
+                </div>
+            </div>
+        </article>
+
+        <article class="helpboard bg-green long-box">
+            <div class="helpboard-content">
+                <img src="images/sample6.jpg" alt="sample" class="helpboard-image">
+                <div class="helpboard-text">
+                    <h2 class="helpboard-title"><a href="#">Title</a></h2>
+                    <p class="author text-green">Requester Name</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. In, ex, inventore quasi commodi deleniti corporis magni delectus culpa beatae mollitia est suscipit tempore harum aliquid.</p>
+                </div>
+            </div>
+        </article>
+
+        <article class="helpboard bg-yellow short-box">
+            <div class="helpboard-content">
+                <img src="images/sample7.jpg" alt="sample" class="helpboard-image">
+                <div class="helpboard-text">
+                    <h2 class="helpboard-title"><a href="#">Title</a></h2>
+                    <p class="author text-yellow">Requester Name</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. In, ex, inventore quasi commodi deleniti corporis magni delectus culpa beatae mollitia est suscipit tempore harum aliquid.</p>
+                </div>
+            </div>
+        </article>
+    </main>
+</body>
+</html>
---
+++ pages/request_1.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Request title</title>
+    <link rel="stylesheet" href="css/style.css?v=1.2">
+</head>
+
+<body>
+    <?php include("navbar.php"); ?>
+
+    <main class="req-page">
+        <h1 class="page-header">Request title</h1>
+        <div class="container">
+            <div class="content">
+                <div class="req-info">
+                    <h2>Requester Name</h2>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus blanditiis at quia quo molestiae fugiat. Neque quod qui similique id consectetur excepturi! Maxime iure magnam rerum incidunt! Minus, deserunt soluta?</p>
+                </div>
+
+                <div class="contact-info">
+                    <h2>Support Requester!</h2>
+                    <p>Contact information: +639 123 456 789</p>
+                    <p>Email: juandelacruz@gmail.com</p>
+                </div>
+            </div>
+
+            <div class="image-container">
+                <img src="images/sample.jpg" alt="Request">
+            </div>
+
+        </div>
+
+        <a class="button-wrapper"><button class="button">Donate</button></a>
+    </main>
+
+    
+</body>
+</html>
