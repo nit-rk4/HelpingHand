8fc8d5ba9de571bcc60ade2c8389ea4b7da02eb3
Denise Ruth Manalang <manalangdeniseruth@gmail.com>
2025-07-08 09:46:36 +0800

    Moved to tambytes database

--- php/config.php
+++ php/config.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = mysqli_connect('localhost', 'root', '', 'helpinghand_db');
+    $conn = mysqli_connect('database.tambytes.cloud', 'helpinghand_db', 'techit', 'helpinghand_db');
     if (!$conn){
         die('Database connection error: '. mysqli_connect_error());
     }
