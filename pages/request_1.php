52c2a09e9773af69c66a83aa56cdac4bb4b6cfb3
Denise Ruth Manalang <manalangdeniseruth@gmail.com>
2025-07-11 16:11:52 +0800

    Updated css link and page title

--- pages/request_1.php
+++ pages/request_1.php
@@ -56,8 +56,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Request title</title>
-    <link rel="stylesheet" href="../css/style.css?v=1.2">
+    <title><?= htmlspecialchars($request['title'])?></title>
+    <link rel="stylesheet" href="../css/style.css?v=1.3">
 </head>
 
 <body>
