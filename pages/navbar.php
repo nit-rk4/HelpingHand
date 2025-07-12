b1a6b0440c5f44a26b69fce1c8184d772e82d540
Denise Ruth Manalang <manalangdeniseruth@gmail.com>
2025-07-12 15:01:12 +0800

    Added dashboard

--- pages/navbar.php
+++ pages/navbar.php
@@ -3,6 +3,11 @@
     <nav>
         <ul class="nav-links">
             <li><a href="/pages/help_board.php">Home</a></li>
+            <?php if ($_SESSION['auth']['type'] === 'admin'): ?>
+                <li><a href="/pages/admin/admin_requests.php">Dashboard</a></li>
+            <?php else: ?>
+                <li><a href="/pages/admin/user_requests.php">Dashboard</a></li>
+            <?php endif;?>
             <li><a href="/pages/about-us.php">About us</a></li>
             <li><a href="/php/logout.php">Log Out</a></li>
         </ul>
