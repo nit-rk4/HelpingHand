fc97bcb073b9942f76a84989c07faa0163b1b001
Denise Ruth Manalang <manalangdeniseruth@gmail.com>
2025-07-16 20:28:43 +0800

    Changed database

--- php/config.php
+++ php/config.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = mysqli_connect('database.tambytes.cloud', 'helpinghand_db', 'techit', 'helpinghand_db');
+    $conn = mysqli_connect('mysql-helpinghand.alwaysdata.net', '423063_user', 'techitorleaveit', 'helpinghand_db');
     if (!$conn){
         die('Database connection error: '. mysqli_connect_error());
     }
